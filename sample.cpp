3a1fcae908da1625a70aa391611f34bf3067b85c
Trtox <52636543+Trtox@users.noreply.github.com>
2021-01-04 17:59:59 +0100

    1st

---
+++ sample.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define elif else if
+#define endl "\n"
+#define eb(x) emplace_back(x)
+#define maxelem(v) *max_element(v.begin(),v.end())
+#define minelem(v) *min_element(v.begin(),v.end())
+#define FOR(a,b) for(int i=int(a);i<int(b);++i)
+#define cend cout<<endl;
+
+using namespace std;
+
+const int mod = 1e9+7;
+
+void izlaz(ll br)
+{
+  cout<<br;
+  exit(0);
+
+}
+
+int main()
+{
+  ios_base :: sync_with_stdio(false);
+  cin.tie(NULL);
+  
+
+
+ return 0;
+}
